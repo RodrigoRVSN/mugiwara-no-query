6c2713d7ac3b23e886d3527dec098993d8eab12d
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-08-20 16:02:48 -0300

    refactor: move button see more infinite scroll to a component

--- src/components/elements/PostsList/index.tsx
+++ src/components/elements/PostsList/index.tsx
@@ -1,12 +1,10 @@
-import { Button, Flex, Spinner } from '@chakra-ui/react'
+import { Flex, Spinner } from '@chakra-ui/react'
 import { useInfiniteQuery } from '@tanstack/react-query'
-import { useEffect } from 'react'
-import { useInView } from 'react-intersection-observer'
 import PostsService from '@App/core/services/PostsService'
+import { ButtonSeeMore } from './components/ButtonSeeMore'
 import { Post } from './components/Post'
 
 export const PostsList = () => {
-  const { ref, inView } = useInView()
   const { data, isLoading, fetchNextPage, hasNextPage } = useInfiniteQuery(['posts'],
     PostsService.getPosts,
     {
@@ -15,29 +13,20 @@ export const PostsList = () => {
     }
   )
 
-  useEffect(() => {
-    if (inView) {
-      fetchNextPage()
-    }
-  }, [fetchNextPage, inView])
-
   if (isLoading) return <Spinner color='blue.500' size='xl' />
 
   return (
     <Flex flexDirection='column' >
       {data?.pages.map(page =>
         page?.map(item => (
-            <Post post={item} key={item.id} />
+          <Post post={item} key={item.id} />
         ))
       )}
 
-      <Button
-        ref={ref}
-        onClick={() => fetchNextPage()}
-        disabled={!hasNextPage}
-      >
-        Carregar mais
-      </Button>
+      <ButtonSeeMore
+        hasNextPage={hasNextPage}
+        fetchNextPage={fetchNextPage}
+      />
     </Flex>
   )
 }
